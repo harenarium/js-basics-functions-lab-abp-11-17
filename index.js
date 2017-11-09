ca213d6e5533cd717c6fb1951d66706c0df3e6f7
harenarium <carolinel2610@gmail.com>
2017-11-09 00:39:13 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Code your solution in this file!
+function distanceFromHqInBlocks(pickupBlock){
+  let blocksHQ = math.abs(pickupBlock-42);
+  return blocksHQ;
+}
+
+function distanceFromHqInFeet(){
+  let feetHQ = distanceFromHqInBlocks()*264;
+  return feetHQ;
+}
+
+function distanceTravelledInFeet
+calculatesFarePrice
